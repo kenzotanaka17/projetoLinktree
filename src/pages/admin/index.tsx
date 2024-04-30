e41f1f17c158035a132e16d83febb0aeeccd5647
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-04-30 11:07:05 -0300

    feat: Finalizando o formulário

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { FiTrash } from "react-icons/fi";
 
 export function Admin() {
     
@@ -48,16 +49,39 @@ export function Admin() {
                     </div>
                 </section>
 
+                {nameInput !== '' && (
                 <div className="flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
                     <label className="text-white font-medium mt-2 mb-3">Preview do link:</label>
                     <article 
                     className="w-11/12 max-w-lg flex flex-col items-center justify-between rounded px-1 py-3 bg-zinc-800"
                     style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput}}
                     >
-                        <p className="font-medium" style={{ color: textColorInput}}>Canal do Youtube</p>
+                        <p className="font-medium" style={{ color: textColorInput}}>{nameInput}</p>
                     </article>
                 </div>
+                )}
+
+                <button type="submit" className="mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center">
+                    Cadastrar
+                </button>
             </form>
+
+            <h2 className="font-bold text-white mb-4 text-2xl">
+                Meus links
+            </h2>
+            <article 
+            className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none" 
+            style={{backgroundColor: '#2563eb', color: '#FFF'}}
+            >
+                <p>
+                    Canal do youtube
+                </p>
+                <div>
+                    <button className="border border-dashed p-1 rounded bg-red-600">
+                        <FiTrash size={18} color="#fff" />
+                    </button>
+                </div>
+            </article> 
         </div>
     )
 }
