e08c3c1e3bdb571889a5149a4c7bdbe80eaca74f
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-04-30 11:47:29 -0300

    feat: Exibindo links

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -80,6 +80,11 @@ export function Admin() {
 
     }
 
+    async function handleDeleteLink(id: string) {
+        const docRef = doc(db, "links", id);
+        await deleteDoc(docRef);
+    }
+
     return(
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header />
@@ -139,19 +144,23 @@ export function Admin() {
             <h2 className="font-bold text-white mb-4 text-2xl">
                 Meus links
             </h2>
-            <article 
-            className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none" 
-            style={{backgroundColor: '#2563eb', color: '#FFF'}}
-            >
-                <p>
-                    Canal do youtube
-                </p>
-                <div>
-                    <button className="border border-dashed p-1 rounded bg-red-600">
-                        <FiTrash size={18} color="#fff" />
-                    </button>
-                </div>
-            </article> 
+            
+            {links.map( (link) => (
+                <article 
+                key={link.id}
+                className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none" 
+                style={{backgroundColor: link.bg, color: link.color}}
+                >
+                    <p>
+                        {link.name}
+                    </p>
+                    <div>
+                        <button className="border border-dashed p-1 rounded bg-red-600" onClick={ () => handleDeleteLink(link.id) }>
+                            <FiTrash size={18} color="#fff" />
+                        </button>
+                    </div>
+                </article> 
+            ) )}
         </div>
     )
 }
